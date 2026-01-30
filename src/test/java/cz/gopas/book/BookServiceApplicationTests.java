462fbf3f86afdf004115915d0c4f5a3c8392f26d
Aleš Roszoha <a.roszoha@gmail.com>
2026-01-30 14:23:38 +0100

    Some sample tests

--- src/test/java/cz/gopas/book/BookServiceApplicationTests.java
+++ src/test/java/cz/gopas/book/BookServiceApplicationTests.java
@@ -1,13 +1,36 @@
 package cz.gopas.book;
 
+import cz.gopas.book.bean.BookDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
 class BookServiceApplicationTests {
+
+    @Autowired
+    private RestTestClient testClient;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testGetAllBooks() { // GET /v1/books/ => 200
+        testClient.get().uri("/v1/books")
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Test
+    void testGetFirstBook() { // GET /v1/books/1 => 200 + Book JSON
+        testClient.get().uri("/v1/books/1")
+                .exchange()
+                .expectBody(BookDTO.class)
+                .consumeWith(resp -> "Java Definitive Guide2".equals(resp.getResponseBody().getTitle()));
     }
 
 }
